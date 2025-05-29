5a1bc5a8593335da3149c7f958f2dfb5e9ffebc9
Bushra Yousaf <smileforbushi@gmail.com>
2025-05-29 16:15:55 +0500

    Update specific files

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,8 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // 👈 Add this line
   plugins: [react()],
   resolve: {
     alias: {
